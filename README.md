05e5e42b5add7521bf23d371ba7012aa86bb13ab
Chris Usick <christopher.usick@gmail.com>
2021-08-31 14:16:09 -0500

    react-boostrap

--- README.md
+++ README.md
@@ -2,11 +2,16 @@
 
 Evaluating different react patterns.
 - all demos use typescripts and smoke tests
-- [ ] typescript and smoke tests
+- [x] typescript and smoke tests
   - repo: ts-smoke-tests
   - basic example with a counter counter component 
   - uses `useState` hook
 - [ ] bootstrap 
+  - [x] react-bootstrap 
+    - very tedious to use all the components
+    - noticed that Navbar.Brand didn't behave the way `.navbar-brand` in bootstrap behaved, with regards to text color
+    - have to navigate between 2 docs: bootstrap and react-boostrap
+  - vanilla-bootstrap
 - [ ] tailwind 
 - [ ] auth with hooks 
 - [ ] http calls 
--- ts-react-bootstrap/src/App.tsx
+++ ts-react-bootstrap/src/App.tsx
@@ -1,14 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import './App.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import NavBar from 'react-bootstrap/NavBar'
+import Navbar from 'react-bootstrap/Navbar'
+import Home from './Home'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>TS React Bootstrap</h1>
-      </header>
+  const ret = (
+    <div>
+      <NavBar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      </NavBar>
+      <Container fluid>
+        <Row>
+          <main>
+            <Home />
+          </main>
+        </Row>
+      </Container>
     </div>
   );
+  // let ret = <Container fluid><Home/></Container>
+  return ret
 }
 
 export default App;
