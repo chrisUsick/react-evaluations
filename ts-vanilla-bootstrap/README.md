ddcfe91263261d328766f59ebe2f6f869c763095
Chris Usick <christopher.usick@gmail.com>
2021-09-15 13:47:27 -0500

    added home page

--- ts-vanilla-bootstrap/README.md
+++ ts-vanilla-bootstrap/README.md
@@ -2,6 +2,11 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Bootstrap 
+
+- npm `i -s bootstrap`
+- add `import 'bootstrap/dist/css/bootstrap.min.css';` to `App.tsx`
+
 ## Available Scripts
 
 In the project directory, you can run:
--- ts-vanilla-bootstrap/src/App.tsx
+++ ts-vanilla-bootstrap/src/App.tsx
@@ -2,18 +2,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home'
 
 function App() {
   return (
     <div>
       <div className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#home">React-Vanilla-Bootstrap</a>
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#home">React-Vanilla-Bootstrap</a>
+        </div>
       </div>
       <div className="container-fluid">
         <div className="row">
           <main>
-            <h1>hello world</h1>
-            {/* <Home /> */}
+            <Home />
           </main>
         </div>
       </div>
